d540670b5183c52d2846394b8d3fc280e681edb0
mehrshad <ghamarimehrshad@gmail.com>
2023-03-29 14:14:04 +0330

    adding some chamge in t6

--- t6/client.py
+++ t6/client.py
@@ -2,35 +2,76 @@ import pika
 import uuid
 
 
-class Sender():
-    def __init__(self):
-        self.connection=pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1'))
-        self.channel=self.connection.channel()
-        result=self.channel.queue_declare(queue='',exclusive=True)
-        self.qname=result.method.queue
-        self.channel.basic_consume(queue=self.qname,on_message_callback=self.on_response,auto_ack=True)
+# class Sender():
+#     def __init__(self):
+#         self.connection=pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1'))
+#         self.channel=self.connection.channel()
+#         result=self.channel.queue_declare(queue='',exclusive=True)
+#         self.qname=result.method.queue
+#         self.channel.basic_consume(queue=self.qname,on_message_callback=self.on_response,auto_ack=True)
 
     
-    def on_response(self,ch,method,properties,body):
-        if self.corre_id==properties.correlation_id:
-            self.response=body
+#     def on_response(self,ch,method,properties,body):
+#         if self.corre_id==properties.correlation_id:
+#             self.response=body
 
+
+#     def call(self,n):
+#         self.response=None
+#         self.coree_id=str(uuid.uuid4())
+#         self.channel.basic_publish(exchange='',routing_key='rpc-queue')
+
+
+#         while self.response is None :
+#             self.connection.process_data_events()
+
+#         return int(self.response)
+
+
+
+# send= Sender()
+
+# response=send.call(20)
+
+# print(response)
+
+print('start')
+
+class ClientSender():
+    def __init__(self):
+        self.connection=pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
+        self.ch=self.connection.channel()
+        result=self.ch.queue_declare(queue='',exclusive=True)
+        self.qname=result.method.queue
+        self.ch.basic_consume(queue=self.qname,on_message_callback=self.on_response,auto_ack=True)
+        print('in')
+        # self.ch.start_consuming()
+    
 
     def call(self,n):
         self.response=None
-        self.coree_id=str(uuid.uuid4())
-        self.channel.basic_publish(exchange='',routing_key='rpc-queue')
-
+        self.core_id=str(uuid.uuid4())
+        self.ch.basic_publish(exchange='',routing_key='rpc_queue',
+                              properties=pika.BasicProperties(reply_to=self.qname,correlation_id=self.core_id),body=str(n))
 
         while self.response is None :
             self.connection.process_data_events()
 
-        return int(self.response)
+        return self.response
+
+    
+    def on_response(self,ch,method,properties,body):
+        if self.core_id == properties.correlation_id:
+            self.response=body
 
 
 
-send= Sender()
+print('befor instance')
+send= ClientSender()
 
+print(type(send))
+print('start2')
 response=send.call(20)
+print('start3')
 
 print(response)
--- t6/server.py
+++ t6/server.py
@@ -1,20 +1,22 @@
 import pika
+import time
 
 
-connenction=pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1'))
+
+connenction=pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
 
 channel=connenction.channel()
 
-channel.queue_declare(queue='rpc-queue')
+channel.queue_declare(queue='rpc_queue')
 
-def call_back(ch,method,properties,body):
+def call_back(ch,method,proper,body):
     n=int(body)
     print('processing ')
-
+    time.sleep(5)
     response=n+1
 
-    ch.basic_publish(routing_key=properties.reply_to,
-                          properties=pika.BasicProperties(correlation_id=properties.correlation_id,body=str(response)))
+    ch.basic_publish(exchange='',routing_key=proper.reply_to,
+                          properties=pika.BasicProperties(correlation_id=proper.correlation_id,),body=str(response))
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
@@ -22,7 +24,7 @@ def call_back(ch,method,properties,body):
 
 channel.basic_qos(prefetch_count=1)
 
-channel.basic_consume(queue='rpc-queue',on_message_callback=call_back)
+channel.basic_consume(queue='rpc_queue',on_message_callback=call_back)
 
 print('wating for message')
 
